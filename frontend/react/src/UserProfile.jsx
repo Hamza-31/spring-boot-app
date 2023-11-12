9a6a99a4c73018642921a8e43726f72d2da6e7ac
Hamza <hamza.labzioui@gmail.com>
2023-11-12 01:13:52 +0100

    Create web app with React | CORS, Axios, Fetch Customers & Card Component

---
+++ frontend/react/src/UserProfile.jsx
@@ -0,0 +1,10 @@
+const UserProfile = ()=>{
+    return (
+        <div>
+            <p>John</p>
+            <img src={"https://randomuser.me/api/portraits/thumb/men/75.jpg"} />
+        </div>
+    )
+}
+
+export default UserProfile;
